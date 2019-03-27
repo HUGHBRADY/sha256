ae1792e1034572aec9316a8eccb32c4fbcdd242f
HUGHBRADY <G00338260@gmit.ie>
2019-03-27 19:51:36 +0000

    Padding functioning correctly

--- padfile.c
+++ padfile.c
@@ -9,29 +9,36 @@ union msgblock {
     uint64_t  s[8];
 };
 
+// For tracking where the program is when padding the message.
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]) {
 
     union msgblock M;
 
-    // integer to append at end of msgblock
+    // integer to append at end of msgblock.
     uint64_t nobits = 0;
 
-    // The number of bytes in current msgblock
+    // The number of bytes in current msgblock.
     uint64_t nobytes;
+
+    enum status S = READ;
 
     FILE* f;
 
+    int i;
+
     f = fopen(argv[1], "r");
 
-    while (!feof(f)) {
+    while (S == READ) {
 
         nobytes = fread(M.e, 1, 64, f);
-        printf("Read %211u bytes \n", nobytes);
+        printf("Read %2llu bytes \n", nobytes);
         nobits = nobits + (nobytes * 8);
 
         if (nobytes < 56) {
             printf("I've found a block with less than 55 bytes.\n");
-            // This byte is going to be seven 0's and a 1.
+            // Append 1 to message.
             M.e[nobytes] = 0x80;
             
             while (nobytes < 56) {
@@ -41,11 +48,31 @@ int main(int argc, char *argv[]) {
             }
             
             M.s[7] = nobits;
+            S = FINISH;
+        } else if (nobytes < 64) { 
+            // Set status
+            S = PAD0;
+            // Append 1 to message.
+            M.e[nobytes] = 0x80;
+
+            while (nobytes < 64) {
+                nobytes = nobytes + 1;
+                // Fill the rest with 0's.
+                M.e[nobytes] = 0x00;
+            }
+        } else if (feof(f)) {
+            S = PAD1;
         }
 
-        printf("%llu\n", nobytes);;
-    
     }
+
+    if (S == PAD0 || S == PAD1) { 
+        for (i = 0; i < 56; i++) 
+            M.e[i] = 0x00;
+        M.s[7] = nobits;
+    } 
+    if (S == PAD1) 
+        M.e[0] = 0x80;       
 
     fclose(f);
 
@@ -54,5 +81,5 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     return 0;
-
 }
+
